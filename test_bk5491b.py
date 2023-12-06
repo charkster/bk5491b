cd1c1440a4c49c679cbe9619fd30170df33b1c5f
charkster <31560568+charkster@users.noreply.github.com>
2023-12-05 22:16:16 -0700

    Update test_bk5491b.py

--- test_bk5491b.py
+++ test_bk5491b.py
@@ -1,10 +1,10 @@
 import pyvisa
 from bk5491b import bk5491b
-import time
 
 rm = pyvisa.ResourceManager('@py')
-
+# default baud_rate is 9600, but I manually configured my unit for 19200
 dmm1 = bk5491b(pyvisa_instr=rm.open_resource('ASRL/dev/ttyUSB0::INSTR'),baud_rate=19200)
+
 dmm1.reset()
 print(dmm1.meas_i(nplc=10,rng=20,samples=4))
 print(dmm1.meas_v(nplc=0.1,samples=5))
